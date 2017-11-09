52a24cccbf4228aaf792ac6ebf6d6a51dde14ac4
RunningCoderLee <sprint_l@163.com>
2017-11-09 18:13:02 +0800

    init

---
+++ routes/graphql.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+import { graphql } from 'graphql';
+import demo from '../schemas/demo';
+
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/', (req, res) => {
+  //GraphQL executor
+  graphql(demo, req.body)
+  .then((result) => {
+    res.send(JSON.stringify(result, null, 2));
+  })
+});
+
+
+module.exports = router;
